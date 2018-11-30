070c1b7260de012e2b984ecf2ef8e726cff4314b
Glitch (panama-surf-report) <none>
2018-11-30 03:12:05 +0000

    ⚽️ Updated to a real (inital) AoG

--- public/client.js
+++ public/client.js
@@ -10,22 +10,22 @@ $(function() {
     var surfMin = data.data.wave[0].surf.min;
     var surfMax = data.data.wave[0].surf.max;
     console.log("Surf min: "+ surfMin + " max: " + surfMax);
-    $("#conditions").append("<h4>🏄🏻‍♂️ The surf: " + surfMin + "f - " + surfMax + "f<h4>");
-});
+    $("#conditions").append("<h4>🏄🏻‍♂️ The surf: " + surfMin + " - " + surfMax + " (ft)<h4>");
+  });
   
   //
   // weather
   $.getJSON("https://services.surfline.com/kbyg/spots/forecasts/weather?spotId=584204204e65fad6a770913f&days=6&intervalHours=3", function( data ) {
     var temp = data.data.weather[0].temperature;
     console.log("temp: "+ temp);
-    $("#conditions").append("<h4>🌤 Temprture " + temp + "f</h4>");
+    $("#conditions").append("<h4>🌤 Temprture " + temp + " (F)</h4>");
   });
 
   // wind
   $.getJSON("https://services.surfline.com/kbyg/spots/forecasts/wind?spotId=584204204e65fad6a770913f&days=6&intervalHours=3", function( data ) {
     var direction = data.data.wind[0].direction;
     var speed = data.data.wind[0].speed;
-    console.log("* Wind direction: "+ direction + " speed: " + speed);
-    $("#conditions").append("<h4>💨 Wind direction " + direction + " speed:" + speed + "knots</h4>");
+    console.log("* Wind direction: "+ direction + "° speed: " + speed);
+    $("#conditions").append("<h4>💨 Wind direction " + direction + " speed:" + speed + " (knots)</h4>");
   });
 });
